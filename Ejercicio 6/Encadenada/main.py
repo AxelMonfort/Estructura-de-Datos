b8a888e183a69594c6466a7e3e842e9478357f21
AxelMonfort <82827783+AxelMonfort@users.noreply.github.com>
2022-09-05 12:28:04 -0300

    Delete Ejercicio 6 directory

--- Ejercicio 6/Encadenada/main.py
+++
@@ -1,7 +0,0 @@
-from Encadenada import Cola
-
-if __name__ == '__main__':
-    cola = Cola()
-    cola.insertar(1)
-    cola.insertar(2)
-    cola.recorrer()
--- Ejercicio 6/Secuencial/main.py
+++
@@ -1,9 +0,0 @@
-from Secuencial import Cola
-
-if __name__ == '__main__':
-    cola = Cola(3)
-    cola.insertar(1)
-    cola.insertar(2)
-    cola.insertar(3)
-    cola.suprimir()
-    cola.recorrer()
